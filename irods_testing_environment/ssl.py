db5729d1ccfac73c7f16d4d9ce7d360a44fb65c8
Alan King <alanking@renci.org>
2022-01-03 12:15:45 -0500

    delete everything that isn't __main__ in scripts

--- irods_testing_environment/ssl.py
+++ irods_testing_environment/ssl.py
@@ -194,33 +194,3 @@ def configure_ssl_in_zone(docker_client, compose_project):
         os.unlink(key_file)
         os.unlink(cert_file)
         os.unlink(dhparams_file)
-
-
-if __name__ == "__main__":
-    import argparse
-    import compose.cli.command
-    import docker
-
-    import cli
-    import logs
-
-    parser = argparse.ArgumentParser(description='Configure SSL in a running iRODS Zone.')
-
-    cli.add_common_args(parser)
-    cli.add_compose_args(parser)
-
-    args = parser.parse_args()
-
-    docker_client = docker.from_env()
-
-    compose_project = compose.cli.command.get_project(os.path.abspath(args.project_directory),
-                                                      project_name=args.project_name)
-
-    logs.configure(args.verbosity)
-
-    try:
-        configure_ssl_in_zone(docker_client, compose_project)
-
-    except Exception as e:
-        logging.critical(e)
-        exit(1)
