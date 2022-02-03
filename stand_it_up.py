b95bb804b23630481c3932303a895013478d2d91
Alan King <alanking@renci.org>
2022-02-03 12:40:08 -0500

    [#38] Add CSC count option to stand_it_up

--- stand_it_up.py
+++ stand_it_up.py
@@ -23,6 +23,12 @@ if __name__ == "__main__":
     cli.add_database_config_args(parser)
     cli.add_irods_package_args(parser)
 
+    parser.add_argument('--consumer-instance-count',
+                        metavar='IRODS_CATALOG_SERVICE_CONSUMER_INSTANCE_COUNT',
+                        dest='consumer_count', type=int, default=0,
+                        help=textwrap.dedent('''\
+                            Number of iRODS Catalog Service Consumer service instances.'''))
+
     parser.add_argument('--use-ssl',
                         dest='use_ssl', action='store_true',
                         help=textwrap.dedent('''\
@@ -51,7 +57,7 @@ if __name__ == "__main__":
                              package_directory=args.package_directory,
                              package_version=args.package_version,
                              odbc_driver=args.odbc_driver,
-                             consumer_count=3)
+                             consumer_count=args.consumer_count)
 
     if args.use_ssl:
         ssl.configure_ssl_in_zone(ctx.docker_client, ctx.compose_project)
