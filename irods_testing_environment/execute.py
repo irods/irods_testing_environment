c4faf6c656d0bd6355473c5b34db607452ec058f
Alan King <alanking@renci.org>
2022-03-10 11:37:12 -0500

    [#68] Rework stream_output arg in execute_command
    stream_output used to be a boolean value which would force the global
    log level to INFO and stream the output of the command being executed.
    We now accept (and default to) None to indicate that the configured log
    level should dictate whether or not the output should be streamed. If
    the output is explicitly set to stream, it will always stream at log
    level ERROR (to ensure that it always shows up regardless of configured
    log level). If the output is explicitly set not to stream, it will never
    stream the output, even if the configured log level is at its highest
    level.

--- irods_testing_environment/execute.py
+++ irods_testing_environment/execute.py
@@ -7,30 +7,43 @@ import os
 # local modules
 from . import context
 
-def execute_command(container, command, user='', workdir=None, stream_output=False):
+def execute_command(container, command, user='', workdir=None, stream_output=None):
+    """Execute `command` in `container` as `user` in `workdir`.
+
+    Running this is equivalent to the following:
+        docker exec -u <user> -w <workdir> <container> <command>
+
+    Arguments:
+    container -- container in which the command will be run
+    command -- string representing the command to run
+    user -- the user whose identity will be assumed when running the command (default: root)
+    workdir -- the present working directory for the command (default: root directory)
+    stream_output -- a boolean value to indicate that output from the command execution should
+                     be streamed to the logging module. If None is used, a decision is made for
+                     the user: If the log level is set to INFO or higher (INFO, DEBUG) then the
+                     output will be streamed. Otherwise, the output will stream no matter what
+                     if True and it will not stream no matter what if False.
+    """
     OUTPUT_ENCODING = 'utf-8'
 
     logging.debug('executing on [{0}] [{1}]'.format(container.name, command))
 
+    if stream_output is None:
+        log_level = logging.getLogger().getEffectiveLevel()
+        stream_output = log_level <= logging.INFO
+
     exec_instance = container.client.api.exec_create(container.id, command, user=user, workdir=workdir)
     exec_out = container.client.api.exec_start(exec_instance['Id'], stream=stream_output)
-
-    previous_log_level = logging.getLogger().getEffectiveLevel()
-    if previous_log_level > logging.INFO:
-        logging.getLogger().setLevel(logging.INFO)
 
     try:
         # Stream output from the executing command. A StopIteration exception is raised
         # by the generator returned by the docker-py API when there is no more output.
         while stream_output:
             out = next(exec_out).decode(OUTPUT_ENCODING)
-            logging.info(out)
+            logging.error(out)
 
     except StopIteration:
         logging.debug('done')
-
-    finally:
-        logging.getLogger().setLevel(previous_log_level)
 
     if not stream_output:
         logging.debug(exec_out.decode(OUTPUT_ENCODING))
