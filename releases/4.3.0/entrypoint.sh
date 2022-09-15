a6b2955da33effad3a01edabdfc898cb41dd51a3
Alan King <alanking@renci.org>
2022-09-15 15:35:55 -0400

    [ 110] Add pre-baked 4.3.0/4.2.11 release images
    The pre-baked images, once built, are set up once when the image is run
    as a container and then is completely restartable. The packages are
    installed at build time and they will never move because the packages
    have already been released.
    
    4.3.0 images are based on ubuntu:20.04 and postgres:12.
    
    4.2.11 images are based on ubuntu:18.04 and postgres:10.12.

---
+++ releases/4.3.0/entrypoint.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+set -e
+
+setup_input_file=/irods_setup.input
+
+if [ -e "${setup_input_file}" ]; then
+    python3 /var/lib/irods/scripts/setup_irods.py < "${setup_input_file}"
+    rm /irods_setup.input
+fi
+
+cd /usr/sbin
+su irods -c 'bash -c "./irodsServer -u"'
