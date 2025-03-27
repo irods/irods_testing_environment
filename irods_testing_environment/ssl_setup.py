1dd8b2fdb46894612325f08d4698ee246297c28b
Alan King <alanking@renci.org>
2025-03-27 13:05:22 -0400

    [irods/irods#2626] iRODS 5.0: Update SSL/TLS configuration

--- irods_testing_environment/ssl_setup.py
+++ irods_testing_environment/ssl_setup.py
@@ -11,6 +11,7 @@ from cryptography.x509.oid import NameOID
 # local modules
 from . import context
 from . import execute
+from . import irods_config
 from . import json_utils
 
 def generate_ssl_certificate_key(directory=None):
@@ -84,11 +85,65 @@ def generate_ssl_dh_params(generator=2, key_size=1024, directory=None):
     return dhfile
 
 
-def configure_ssl_on_server(container,
-                            path_to_key_file_on_host,
-                            path_to_cert_file_on_host,
-                            path_to_dhparams_file_on_host):
-    """Copy SSL files to the container and configure SSL on the iRODS server.
+def configure_tls_for_service_account(container, cert_file):
+    """Configure TLS for the iRODS service account client environment.
+
+    Arguments:
+    container -- the docker.Container on which SSL will be configured
+    cert_file -- path to the file in the container containing the self-signed cert
+    """
+    logging.warning(f'[{container.name}]: configuring SSL')
+
+    # add certificate chain file, certificate key file, and dh parameters file to iRODS
+    # service account environment file
+    service_account_irods_env = os.path.join(context.irods_home(), '.irods', 'irods_environment.json')
+    irods_env = json_utils.get_json_from_file(container, service_account_irods_env)
+
+    logging.debug(f'[{container.name}: env before [{json.dumps(irods_env)}]')
+
+    irods_env['irods_client_server_policy'] = 'CS_NEG_REQUIRE'
+    irods_env['irods_ssl_ca_certificate_file'] = cert_file
+    irods_env['irods_ssl_verify_server'] = 'cert'
+
+    logging.debug(f'[{container.name}: env after [{json.dumps(irods_env)}]')
+
+    json_utils.put_json_to_file(container, service_account_irods_env, irods_env)
+
+
+def configure_tls_in_server_config(container, key_file, cert_file, dhparams_file):
+    """Configure TLS in server_config on the iRODS server.
+
+    Arguments:
+    container -- the docker.Container on which SSL will be configured
+    key_file -- path to the file in the container containing the private key for the cert
+    chain_file -- path to the file in the container containing the self-signed cert
+    dhparams_file -- path to the file in the container containing the dhparams PEM file
+    """
+    from . import negotiation_key
+
+    config = json_utils.get_json_from_file(container, context.server_config())
+
+    logging.debug(f'[{container.name}: config before [{json.dumps(config)}]')
+
+    config["tls"] = {
+        "certificate_chain_file": chain_file,
+        "certificate_key_file": key_file,
+        "dh_params_file": dhparams_file
+    }
+
+    json_utils.put_json_to_file(container, context.server_config(), config)
+
+    logging.debug(f'[{container.name}: config after [{json.dumps(config)}]')
+
+    negotiation_key.backup_file(container, context.core_re())
+    negotiation_key.configure_ssl_in_server(container, 'CS_NEG_REQUIRE')
+
+
+def configure_ssl_on_irods4_server(container,
+                                   path_to_key_file_on_host,
+                                   path_to_cert_file_on_host,
+                                   path_to_dhparams_file_on_host):
+    """Copy SSL files to the container and configure SSL on the iRODS 4 server.
 
     Arguments:
     container -- the docker.Container on which SSL will be configured
@@ -140,6 +195,60 @@ def configure_ssl_on_server(container,
     # TODO: consider using a generator to restore the file here...
     negotiation_key.backup_file(container, context.core_re())
     negotiation_key.configure_ssl_in_server(container, 'CS_NEG_REQUIRE')
+
+    # start the server again
+    start_cmd = "python3 -c 'from scripts.irods.controller import IrodsController; IrodsController().start()'"
+    if execute.execute_command(container, start_cmd, user='irods', workdir=context.irods_home()) is not 0:
+        raise RuntimeError(
+            'failed to start iRODS server after SSL configuration [{}]'
+            .format(container.name))
+
+    logging.warning('SSL configured successfully [{}]'.format(container.name))
+
+
+def configure_ssl_on_server(container,
+                            path_to_key_file_on_host,
+                            path_to_cert_file_on_host,
+                            path_to_dhparams_file_on_host):
+    """Copy SSL files to the container and configure SSL on the iRODS server.
+
+    Arguments:
+    container -- the docker.Container on which SSL will be configured
+    path_to_key_file_on_host -- path to file on host containing the private key for the cert
+    path_to_cert_file_on_host -- path to file on host containing the self-signed cert
+    path_to_dhparams_file_on_host -- path to file on host containing the dhparams PEM file
+    """
+    # If this is not an iRODS 5 server, use the old way of configuring SSL.
+    version = irods_config.get_irods_version(container)
+    if int(version[0]) < 5 and int(version[1]) < 90:
+        return configure_ssl_on_irods4_server(
+            container, path_to_key_file_on_host, path_to_cert_file_on_host, path_to_dhparams_file_on_host)
+
+    from . import archive
+    from . import negotiation_key
+
+    key_file = os.path.join(context.irods_config(), 'server.key')
+    dhparams_file = os.path.join(context.irods_config(), 'dhparams.pem')
+    chain_file = os.path.join(context.irods_config(), 'chain.pem')
+    cert_file = os.path.join(context.irods_config(), 'server.crt')
+
+    stop_cmd = "python3 -c 'from scripts.irods.controller import IrodsController; IrodsController().stop()'"
+    if execute.execute_command(container, stop_cmd, user='irods', workdir=context.irods_home()) is not 0:
+        raise RuntimeError(
+            'failed to stop iRODS server before SSL configuration [{}]'
+            .format(container.name))
+
+    logging.warning('configuring SSL [{}]'.format(container.name))
+
+    archive.copy_files_in_container(container,
+                                    [(path_to_key_file_on_host, key_file),
+                                     (path_to_cert_file_on_host, chain_file),
+                                     (path_to_cert_file_on_host, cert_file),
+                                     (path_to_dhparams_file_on_host, dhparams_file)])
+
+    configure_tls_for_service_account(container, cert_file)
+
+    configure_tls_in_server_config(container, key_file, chain_file, dhparams_file)
 
     # start the server again
     start_cmd = "python3 -c 'from scripts.irods.controller import IrodsController; IrodsController().start()'"
