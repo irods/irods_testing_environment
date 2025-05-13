5951ae3f33ed64aee4a98f38065755bcfae3ef6c
Alan King <alanking@renci.org>
2025-05-13 16:18:47 -0400

    [#260] Return 'python3' for container_info.python

--- irods_testing_environment/container_info.py
+++ irods_testing_environment/container_info.py
@@ -1,7 +1,4 @@
 def python(container):
     """Return command to run python appropriately per detected iRODS version in `container`."""
-    from . import irods_config
-
-    major, minor, patch = irods_config.get_irods_version(container)
-
-    return 'python' if minor < 3 else 'python3'
+    # Just return python3 because all supported platforms use this... for now...
+    return "python3"
