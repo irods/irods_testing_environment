d1c913c7c2e371d020671941569f509df2b9ce40
Alan King <alanking@renci.org>
2026-02-06 07:58:26 -0500

    [#193,#196] Fix collecting logs in run_plugin_tests
    This should also fix cleaning up containers when collecting said logs
    does fail.

--- run_plugin_tests.py
+++ run_plugin_tests.py
@@ -138,18 +138,18 @@ finally:
                                                   [os.path.join(context.irods_home(), 'test-reports')],
                                                   output_directory)
 
+            # ...and then the extra logs.
+            if args.extra_logs_path:
+                archive.collect_files_from_containers(
+                    ctx.docker_client, ctx.irods_containers(), [args.extra_logs_path], output_directory
+                )
+
         except Exception as e:
             logging.error(e)
             logging.error('failed to collect some log files')
 
             if rc == 0:
                 rc = 1
-
-    if args.extra_logs_path:
-        try:
-            logs.collect_logs(ctx.docker_client, ctx.irods_containers(), output_directory, logfile_path = args.extra_logs_path)
-        except docker.errors.NotFound:
-            logging.warning('Path in container not found for --extra-logs-path {!r}'.format(args.extra_logs_path))
 
     if args.cleanup_containers:
         ctx.compose_project.down(include_volumes=True, remove_image_type=False)
